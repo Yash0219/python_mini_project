cf21c18b55ad3f784fbb2b82d8facbc9eacd2ac5
yash0219 <yashtyagi219@gmail.com>
2025-06-20 23:01:07 +0530

    refactorized code

--- Rock_PAper_Sissor.py
+++ Rock_PAper_Sissor.py
@@ -1,30 +1,47 @@
 import random
 
-emojie = {'r': '🪨', 's': '✂️', 'p': '📃'}  
-#using tuple cause immutable
-choices = ('r', 'p', 's') 
+ROCK = 'r'
+SCISSORS = 's'
+PAPER = 'p'
+emojis = { ROCK: '🪨', SCISSORS: '✂️', PAPER: '📃' }
+choices = tuple(emojis.keys())
 
-while True:
-    user_choice = input("rock, paper or scissor (r,p,s): ").lower()
-    
-    if user_choice not in choices:
-        print("Invalid choice!")
-        continue
-        
-    computer_choice = random.choice(choices)
-    
-    print(f'You chose: {emojie[user_choice]}')
-    print(f'Computer chose: {emojie[computer_choice]}')
-    
-    if user_choice == computer_choice:
-        print("It's a tie!")
-    elif (user_choice == 'r' and computer_choice == 'p') or \
-         (user_choice == 'p' and computer_choice == 's') or \
-         (user_choice == 's' and computer_choice == 'r'):
-        print("Computer wins!!")
+def get_user_choice():
+  while True:
+    user_choice = input('Rock, paper, or scissors? (r/p/s): ').lower()
+    if user_choice in choices:
+      return user_choice      
     else:
-        print("You win!!") 
-        
-    check = input("Do you want to continue? (y/n): ").lower()
-    if check == 'n':
-        break
+      print('Invalid choice!')
+      
+def display_choices(user_choice, computer_choice):
+  print(f'You chose {emojis[user_choice]}')
+  print(f'Computer chose {emojis[computer_choice]}')
+
+
+def determine_winner(user_choice, computer_choice):
+  if user_choice == computer_choice:
+    print('Tie!')
+  elif (
+    (user_choice == ROCK and computer_choice == SCISSORS) or 
+    (user_choice == SCISSORS and computer_choice == PAPER) or 
+    (user_choice == PAPER and computer_choice == ROCK)):
+    print('You win')
+  else:
+    print('You lose')  
+
+def play_game():
+  while True:
+    user_choice = get_user_choice()
+
+    computer_choice = random.choice(choices)
+
+    display_choices(user_choice, computer_choice)
+
+    determine_winner(user_choice, computer_choice)
+
+    should_continue = input('Continue? (y/n): ').lower()
+    if should_continue == 'n':
+      break
+
+play_game()
